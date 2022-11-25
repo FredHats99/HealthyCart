39c9fa74eeef703773908eafc5aa6d667bce818e
FredHats99 <federico.cappellini99@gmail.com>
2022-11-25 11:06:17 +0100

    Created class supermarket, not sure how it will work, need to check Google API for positions and distances first.

---
+++ src/main/java/com/cappellinispirito/ispw_project_202223_jfx/Supermarket.java
@@ -0,0 +1,18 @@
+package com.cappellinispirito.ispw_project_202223_jfx;
+
+public class Supermarket {
+    //variables
+    private final String supermarketName;
+    private final String address;
+    private final int addressNumber;
+    //constructor
+    public Supermarket(String name, String address, int addressNumber){
+        this.supermarketName = name;
+        this.address = address;
+        this.addressNumber = addressNumber;
+    }
+    //methods
+    //public getCoordinateFrom(String address){}
+
+    //public calculateDistance(float userX, float userY, float marketX, float marketY){}
+}
