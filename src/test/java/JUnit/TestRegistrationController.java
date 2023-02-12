9c5df30b07fd41cf4282127f4d569e86f1a91fae
FredHats99 <federico.cappellini99@gmail.com>
2023-02-12 22:01:48 +0100

    selenium

---
+++ src/test/java/JUnit/TestRegistrationController.java
@@ -0,0 +1,37 @@
+package JUnit;
+
+import com.cappellinispirito.ispw_project_202223_jfx.Controller.RegistrationController;
+import com.cappellinispirito.ispw_project_202223_jfx.Model.Exceptions.FailedRegistrationException;
+import com.cappellinispirito.ispw_project_202223_jfx.Model.beansInterface.RegistrationBean;
+import com.cappellinispirito.ispw_project_202223_jfx.View.beans.RegistrationBeanClass;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestRegistrationController {
+
+    private static final String NONREGISTEREDUSER = "Cappellini";
+    private static final String NONREGISTEREDPASSWORD = "Federico";
+
+    @Test
+    void testRegistrationNonExistingUser() throws Exception {
+
+        RegistrationBean registrationBean = new RegistrationBeanClass();
+        boolean result;
+        registrationBean.setUsername(NONREGISTEREDUSER);
+        registrationBean.setPassword(NONREGISTEREDPASSWORD);
+
+        RegistrationController registrationController = new RegistrationController();
+        try{
+            registrationController.register(registrationBean);
+            result = true;
+        } catch (SQLIntegrityConstraintViolationException e) {
+            result = false;
+        }
+
+        Assertions.assertTrue(result);
+    }
+}
---
+++ src/test/java/Selenium/TestSearchProduct.java
@@ -0,0 +1,51 @@
+package Selenium;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+
+public class TestSearchProduct {
+
+    private WebDriver driver;
+    private static final String SEARCH_INPUT = "searchInput";
+    JavascriptExecutor js;
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Before
+    public void setUp() {
+        System.setProperty("C:webdriver.chrome.driver","C:\\Users\\Paola\\IdeaProjects\\ISPW_Project_22-23_JFX\\src\\test\\java\\selenium\\Driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+    }
+
+    @Test
+    public void testSearchProduct() {
+        // Test name: testSpidermanAuthor
+        // Step # | name | target | value | comment
+        // 1 | open | /wiki/Pagina_principale |  |
+        driver.get("https://it.wikipedia.org/wiki/Pagina_principale");
+        // 2 | setWindowSize | 945x1012 |  |
+        driver.manage().window().setSize(new Dimension(945, 1012));
+        // 3 | click | id=searchInput |  |
+        driver.findElement(By.id(SEARCH_INPUT)).click();
+        // 4 | type | id=searchInput | uomo ragno |
+        driver.findElement(By.id(SEARCH_INPUT)).sendKeys("uomo ragno");
+        // 5 | sendKeys | id=searchInput | ${KEY_ENTER} |
+        driver.findElement(By.id(SEARCH_INPUT)).sendKeys(Keys.ENTER);
+        // 6 | runScript | window.scrollTo(0,495) |  |
+        js.executeScript("window.scrollTo(0,495)");
+        // 7 | assertText | linkText=Stan Lee | Stan Lee |
+        assertThat(driver.findElement(By.linkText("Stan Lee")).getText(), is("Stan Lee"));
+        }
+}
